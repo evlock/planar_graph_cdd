a80a28facf3c59cb9763e819e8f0ba9e226d78a3
evlock <elockwood@olin.edu>
2023-10-29 11:36:02 -0400

    Add basic visualization

--- DFS_graph_class.py
+++ DFS_graph_class.py
@@ -1,3 +1,6 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 class Graph:
     def __init__(self, num_vertices):
         self.time = 0
@@ -65,17 +68,42 @@ class Graph:
 
             self.end_time[node] = self.time
             self.time += 1
+            
+    def visualize_graph_dfs(self, graph_obj):
+        G = nx.Graph()
+        for v in range(graph_obj.num_vertices):
+            G.add_node(v)
+            for u in graph_obj.graph[v]:
+                G.add_edge(v, u)
+
+        pos = nx.spring_layout(G)  # Positions for all nodes
+
+        plt.figure(figsize=(8, 6))
+
+        # Draw the graph
+        nx.draw(G, pos, with_labels=True, node_size=500, node_color='lightblue', font_weight='bold')
+
+        # Get node colors for DFS traversal path
+        node_colors = ['lightblue' for _ in range(graph_obj.num_vertices)]
+        for node in graph_obj.traversal_array:
+            node_colors[node] = 'orange'  # Color the nodes in the DFS traversal path
+
+        # Highlight the DFS traversal path
+        nx.draw_networkx_nodes(G, pos, node_color=node_colors, node_size=500)
+        nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+
+        plt.title('Graph Visualization with DFS Traversal')
+        plt.show()
 
 
-G = Graph(5)
+G = Graph(7)
 G.add_edge(0, 1)
-G.add_edge(1, 2)
-G.add_edge(2, 3)
-G.add_edge(3, 1)
-G.add_edge(4, 1)
+G.add_edge(0, 2)
+G.add_edge(1, 3)
+G.add_edge(1, 4)
+G.add_edge(2, 5)
+G.add_edge(2, 6)
 print(f"1: {G.graph[1]}")
 
 G.dfs()
-
-
-        
+G.visualize_graph_dfs(G)
